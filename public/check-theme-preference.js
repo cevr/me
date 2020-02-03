1a396d2915f840a86ef6aacc357af899170598ad
Cristian Velasquez Ramos <seeve.c@gmail.com>
2020-02-02 21:30:26 -0500

    reduce theme preference script

--- public/check-theme-preference.js
+++ public/check-theme-preference.js
@@ -1,13 +1,7 @@
 (function() {
-  let storageKey = '__LIGHT';
-  let lightModeClassName = 'light';
-  let mql = window.matchMedia('(prefers-color-scheme: light)');
-  let prefersColorSchemeLight = mql.matches;
-  let prefersLightMode = prefersColorSchemeLight;
-  try {
-    prefersLightMode = JSON.parse(localStorage.getItem(storageKey));
-  } catch (err) {}
-  if ((prefersLightMode !== null && prefersLightMode) || prefersColorSchemeLight) {
-    document.body.classList.add(lightModeClassName);
+  let lightColorScheme = window.matchMedia('(prefers-color-scheme: light)');
+  let prefersLightMode = JSON.parse(localStorage.getItem('__LIGHT'));
+  if ((prefersLightMode === null && lightColorScheme.matches) || prefersLightMode) {
+    document.body.classList.add('light');
   }
 })();
