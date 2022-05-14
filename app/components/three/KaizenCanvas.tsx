d7e0ba8c4f609aa91d471e4b555bbc258fc6018e
Cristian Velasquez Ramos <seeve.c@gmail.com>
2022-05-14 18:39:49 -0400

    restrict imported font text characters, add loading spinner

---
+++ app/components/three/KaizenCanvas.tsx
@@ -0,0 +1,87 @@
+import { Html, Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import * as React from "react";
+import { motion } from "framer-motion";
+import type { Group } from "three";
+
+import { Star } from "./Star";
+import { KaizenText } from "./KaizenText";
+
+type KaizenCanvasProps = {
+  stars: {
+    projectId: string;
+    id: string;
+  }[];
+  hoveredProjectId: React.MutableRefObject<string | null>;
+};
+
+export function KaizenCanvas({ stars, hoveredProjectId }: KaizenCanvasProps) {
+  let [textRef, setTextRef] = React.useState<Group | null>(null);
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 15] }}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -1,
+      }}
+    >
+      <Stars />
+
+      <ambientLight intensity={1} color="#fababa" />
+      <React.Suspense fallback={<Progress />}>
+        <group position={[-6, 1, 0]}>
+          {textRef && <directionalLight intensity={2} target={textRef} />}
+          <pointLight intensity={1} position={[0, 1, 2]} />
+          <KaizenText ref={setTextRef} />
+          {stars.map((star) => (
+            <StarInitializer star={star} key={star.id} hoveredProjectId={hoveredProjectId} />
+          ))}
+        </group>
+      </React.Suspense>
+    </Canvas>
+  );
+}
+
+function Progress() {
+  return (
+    <Html center position={[-6, 0, 0]}>
+      <motion.div
+        className="w-12 h-12 border-8 border-gray-600 border-t-[salmon] rounded-[50%]"
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 1 }}
+      />
+    </Html>
+  );
+}
+
+type StarInitializerProps = {
+  star: {
+    projectId: string;
+    id: string;
+  };
+  hoveredProjectId: React.MutableRefObject<string | null>;
+};
+
+function StarInitializer({ star, hoveredProjectId }: StarInitializerProps) {
+  const props = React.useRef({
+    x: (4 + Math.random() * 8) * (Math.round(Math.random()) ? -1 : 1),
+    y: -2 + Math.random() * 4,
+    z: -0.25 + Math.random() * 1,
+    rotationY: Math.random() * Math.PI * 2,
+  });
+  const { x, y, z, rotationY } = props.current;
+
+  return (
+    <Star
+      position={[x, y, z]}
+      rotation={[0, x > 0 ? Math.PI : 0, 0]}
+      hoveredProjectId={hoveredProjectId}
+      projectId={star.projectId}
+      initialRotationY={rotationY}
+    />
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,19 +1,12 @@
-import { Stars } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
 import * as React from "react";
 import type { LinksFunction, LoaderFunction, MetaFunction } from "remix";
-import { json } from "remix";
-import { useLoaderData } from "remix";
-import { ClientOnly } from "remix-utils";
-import type { Group } from "three";
+import { useLoaderData, json } from "remix";
 
 import { ExternalLink } from "~/components";
 import { Star } from "~/components/icons";
-import { Star as ThreeStar } from "~/components/three";
-import { KaizenText } from "~/components/three/KaizenText";
+import { KaizenCanvas } from "~/components/three";
 import { projectsApi } from "~/lib";
 import type { Project } from "~/lib/projects.server";
-
 import indexStyles from "../styles/index.css";
 
 export let meta: MetaFunction = () => {
@@ -61,7 +54,7 @@ export default function Index() {
 
   return (
     <main className="home">
-      <ClientOnly>{() => <KaizenCanvas stars={stars} hoveredProjectId={hoveredProjectId} />}</ClientOnly>
+      <KaizenCanvas stars={stars} hoveredProjectId={hoveredProjectId} />
       <section className="projects z-20">
         <h2> Projects </h2>
         {data.projects.map((project) => (
@@ -91,72 +84,5 @@ export default function Index() {
         ))}
       </section>
     </main>
-  );
-}
-
-type KaizenCanvasProps = {
-  stars: {
-    projectId: string;
-    id: string;
-  }[];
-  hoveredProjectId: React.MutableRefObject<string | null>;
-};
-
-function KaizenCanvas({ stars, hoveredProjectId }: KaizenCanvasProps) {
-  let [textRef, setTextRef] = React.useState<Group | null>(null);
-  return (
-    <Canvas
-      camera={{ position: [0, 0, 15] }}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        zIndex: -1,
-      }}
-    >
-      <Stars />
-
-      <ambientLight intensity={1} color="#fababa" />
-      <React.Suspense fallback={null}>
-        <group position={[-6, 1, 0]}>
-          {textRef && <directionalLight intensity={2} target={textRef} />}
-          <pointLight intensity={1} position={[0, 1, 2]} />
-          <KaizenText ref={setTextRef} />
-          {stars.map((star) => (
-            <StarInitializer star={star} key={star.id} hoveredProjectId={hoveredProjectId} />
-          ))}
-        </group>
-      </React.Suspense>
-    </Canvas>
-  );
-}
-
-type StarInitializerProps = {
-  star: {
-    projectId: string;
-    id: string;
-  };
-  hoveredProjectId: React.MutableRefObject<string | null>;
-};
-
-function StarInitializer({ star, hoveredProjectId }: StarInitializerProps) {
-  const props = React.useRef({
-    x: (4 + Math.random() * 8) * (Math.round(Math.random()) ? -1 : 1),
-    y: -2 + Math.random() * 4,
-    z: -0.25 + Math.random() * 1,
-    rotationY: Math.random() * Math.PI * 2,
-  });
-  const { x, y, z, rotationY } = props.current;
-
-  return (
-    <ThreeStar
-      position={[x, y, z]}
-      rotation={[0, x > 0 ? Math.PI : 0, 0]}
-      hoveredProjectId={hoveredProjectId}
-      projectId={star.projectId}
-      initialRotationY={rotationY}
-    />
   );
 }
