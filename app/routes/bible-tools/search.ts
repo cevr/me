84b0ef83333decfcfa9dde728c72bc983f5a98d7
Cristian Velasquez Ramos <seeve.c@gmail.com>
2023-04-19 10:55:52 -0400

    fix search endpoint

--- app/routes/bible-tools/search.ts
+++ app/routes/bible-tools/search.ts
@@ -1,9 +1,22 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
+type EmbeddingSource = {
+  source: string;
+  label: string;
+};
+
 export let loader = async ({ request }: LoaderArgs) => {
   const query = new URL(request.url).searchParams.get("query") ?? "";
   if (!query) throw new Error("No query provided");
-  const result = await fetch(`https://bible-tools-api-production.up.railway.app/search?q=${query}`);
+  const result = await fetch(`https://bible-tools-api-production.up.railway.app/search?q=${query}`).then(
+    (res) =>
+      res.json() as Promise<{
+        egw: EmbeddingSource[];
+        bible: EmbeddingSource[];
+        answer: string;
+      }>,
+  );
+
   return json(result);
 };
