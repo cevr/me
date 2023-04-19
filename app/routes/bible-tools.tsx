100a95ac6500a5bcb6553ba2342d83b6fb2f9f0f
Cristian Velasquez Ramos <seeve.c@gmail.com>
2023-04-19 19:21:15 -0400

    move input to bottom

--- app/routes/bible-tools.tsx
+++ app/routes/bible-tools.tsx
@@ -21,43 +21,45 @@ export default function EgwSearchPage() {
     bible: Omit<LabeledEmbedding, "embedding">[];
   }>();
   return (
-    <main className="flex flex-col h-full gap-4 justify-center h-min-0">
+    <main className="flex flex-col h-full gap-4 justify-between h-min-0">
+      <div className="flex flex-col gap-2 h-full overflow-y-auto scrollbar-hide">
+        {fetcher.data && (
+          <>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-2xl">Answer</h2>
+              <p>{fetcher.data.answer}</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-2xl">Bible results</h2>
+              <ul className="flex flex-col gap-2">
+                {fetcher.data.bible.map((result) => (
+                  <li key={result.label} className="flex flex-col gap-1">
+                    <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
+                    <span>{result.source}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <h2 className="text-2xl">EGW results</h2>
+              <ul className="flex flex-col gap-2">
+                {fetcher.data.egw.map((result) => (
+                  <li key={result.label} className="flex flex-col gap-1">
+                    <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
+                    <span>{result.source}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
+      </div>
+
       <fetcher.Form method="GET" action="/bible-tools/search" className="flex gap-2">
-        <Input type="text" name="query" placeholder="Study the bible" />
+        <Input type="text" name="query" placeholder="Study the bible" autoComplete="off" />
         <Button type="submit">{fetcher.state !== "idle" ? "Loading..." : "Search"}</Button>
       </fetcher.Form>
-
-      {fetcher.data && (
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col gap-2">
-            <h2 className="text-2xl">Answer</h2>
-            <p>{fetcher.data.answer}</p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h2 className="text-2xl">Bible results</h2>
-            <ul className="flex flex-col gap-2">
-              {fetcher.data.bible.map((result) => (
-                <li key={result.label} className="flex flex-col gap-1">
-                  <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
-                  <span>{result.source}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <h2 className="text-2xl">EGW results</h2>
-            <ul className="flex flex-col gap-2">
-              {fetcher.data.egw.map((result) => (
-                <li key={result.label} className="flex flex-col gap-1">
-                  <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
-                  <span>{result.source}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
     </main>
   );
 }
