d4408fa26f8607d4464923c4b8d7f3aa9479b853
Cristian Velasquez Ramos <seeve.c@gmail.com>
2023-04-29 21:05:26 -0400

    use defer

--- app/routes/bible-tools.tsx
+++ app/routes/bible-tools.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import type { LoaderArgs, MetaFunction } from "@remix-run/node";
 import { defer } from "@remix-run/node";
-import { Await, useFetcher, useLoaderData } from "@remix-run/react";
+import { Await, Form, useLoaderData, useNavigation, useSearchParams } from "@remix-run/react";
 
 import { Button } from "~/components/button";
 import { Input } from "~/components/input";
@@ -19,7 +19,7 @@ export let loader = async ({ request }: LoaderArgs) => {
   const query = new URL(request.url).searchParams.get("query") ?? "";
   if (!query)
     return defer({
-      data: {
+      result: {
         answer: "",
         egw: [] as EmbeddingSource[],
         bible: [] as EmbeddingSource[],
@@ -38,56 +38,62 @@ export let loader = async ({ request }: LoaderArgs) => {
 };
 
 export default function EgwSearchPage() {
-  let fetcher = useFetcher<{
-    result: {
-      answer: string;
-      egw: EmbeddingSource[];
-      bible: EmbeddingSource[];
-    };
-  }>();
+  const [params] = useSearchParams();
+  const queryExists = params.has("query");
+  const formState = useNavigation();
 
-  console.log(fetcher.data);
+  let data = useLoaderData<typeof loader>();
+
+  const loading = formState.state !== "idle";
 
   return (
     <main className="flex flex-col h-full gap-4 justify-between h-min-0">
-      <div className="flex flex-col gap-2 h-full overflow-y-auto scrollbar-hide">
-        {fetcher.data && (
-          <>
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl">Answer</h2>
-              <p>{fetcher.data.result.answer}</p>
-            </div>
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl">Bible results</h2>
-              <ul className="flex flex-col gap-2">
-                {fetcher.data.result.bible.map((result) => (
-                  <li key={result.label} className="flex flex-col gap-1">
-                    <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
-                    <span>{result.source}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
+      <React.Suspense fallback={null}>
+        <Await resolve={data.result}>
+          {(data) => (
+            <div className="flex flex-col gap-2 h-full overflow-y-auto scrollbar-hide">
+              {queryExists ? (
+                <>
+                  <div>
+                    <div className="flex flex-col gap-2">
+                      <h2 className="text-2xl">Answer</h2>
+                      <p>{data.answer}</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h2 className="text-2xl">Bible results</h2>
+                      <ul className="flex flex-col gap-2">
+                        {data.bible.map((result) => (
+                          <li key={result.label} className="flex flex-col gap-1">
+                            <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
+                            <span>{result.source}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
 
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl">EGW results</h2>
-              <ul className="flex flex-col gap-2">
-                {fetcher.data.result.egw.map((result) => (
-                  <li key={result.label} className="flex flex-col gap-1">
-                    <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
-                    <span>{result.source}</span>
-                  </li>
-                ))}
-              </ul>
+                    <div className="flex flex-col gap-2">
+                      <h2 className="text-2xl">EGW results</h2>
+                      <ul className="flex flex-col gap-2">
+                        {data.egw.map((result) => (
+                          <li key={result.label} className="flex flex-col gap-1">
+                            <span className="text-sm text-neutral-300 font-mono">{result.label}</span>
+                            <span>{result.source}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </>
+              ) : null}
             </div>
-          </>
-        )}
-      </div>
+          )}
+        </Await>
+      </React.Suspense>
 
-      <fetcher.Form method="GET" className="flex gap-2">
+      <Form method="GET" className="flex gap-2">
         <Input type="text" name="query" placeholder="Study the bible" autoComplete="off" />
-        <Button type="submit">{fetcher.state !== "idle" ? "Loading..." : "Search"}</Button>
-      </fetcher.Form>
+        <Button type="submit">{loading ? "Loading..." : "Search"}</Button>
+      </Form>
     </main>
   );
 }
