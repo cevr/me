a621be07956461c0ce0951c2eeb41d67d92907a6
Cristian Velasquez Ramos <seeve.c@gmail.com>
2024-01-20 19:47:00 -0500

    use express

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
-import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/express";
 import { installGlobals } from "@remix-run/node";
-import { createRequestHandler } from "@remix-run/vercel";
+import express from "express";
 
 installGlobals();
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? undefined
+    : await import("vite").then((vite) =>
+        vite.createServer({
+          server: { middlewareMode: true },
+        }),
+      );
+
+const app = express();
+
+// handle asset requests
+if (viteDevServer) {
+  app.use(viteDevServer.middlewares);
+} else {
+  app.use(
+    "/assets",
+    express.static("build/client/assets", {
+      immutable: true,
+      maxAge: "1y",
+    }),
+  );
+}
+app.use(express.static("build/client", { maxAge: "1h" }));
+
+// handle SSR requests
+app.all(
+  "*",
+  createRequestHandler({
+    build: viteDevServer
+      ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+      : await import("./build/server/index.js"),
+  }),
+);
+
+const port = 3000;
+app.listen(port, () => console.log("http://localhost:" + port));
